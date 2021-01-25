9bc053979f0083c2ffd65650ec17ecb2c588c050
r0han99 <shikataganai.955@gmail.com>
2021-01-26 03:41:05 +0530

    patch_num-2
    - Added functionalities to pipeline script
    - created variable_dict and prompt_dict
    - now the prompts under the project artefacts are predefined and dynamically generated
    - segmented artefacts and plots into radios
    - added download-link to the source-code pdf

--- main.py
+++ main.py
@@ -7,9 +7,11 @@ import pandas as pd
 # os dependencies
 from pathlib import Path
 import base64
+import pickle
+import glob
 
 # '''
-# paths to the report files 
+# paths to the report files -- Dynamic 
 # pytorch = ?
 # keras = ?  
 
@@ -55,63 +57,37 @@ def model_eval(model):
     
 def cs_body(report):
 
-    # st.code(report.columns)
-
+    cols = report.columns[3:-1]
+    report.fillna('None',inplace=True)
     st.markdown("<h3 style='font-family: Avenir; font-weight:bold; font-size:30px;'>Project Artefacts</h3>",unsafe_allow_html=True)
     st.markdown('***')
-    
-    # Network Type 
 
-    st.markdown('*Network Type : *')
-    st.code(report['network_type'].values[0])
-    # st.markdown("<p style='font-family: century gothic;'>Network Type : {}<p>".format(),unsafe_allow_html=True)
-
-
-    # Prediction Type 
-    st.markdown('*Prediction Type : *')
-    st.code(report['prediction_type'].values[0])
+    with open('./prompt-list.txt', 'rb') as f:
+        data = f.read() 
+    d = pickle.loads(data)
 
     
-
-
-    # Network Architecture Pytorch
-    st.markdown('*Network Architecture : *')
-    st.code(report['framework'].values[0]+'\n'+report['Architecture'].values[0])
-
-
-    # Number of Layers
-    st.markdown('*Number of  Layers: *')
-    st.code(report['layers'].values[0])
-
-    # Number of Hidden Units
-    st.markdown('*Number of Hidden Units: *')
-    if str(report['hidden_units'].values[0]) == 'nan':
-        st.code('None')
-    else:
-        st.code(report['hidden_units'].values[0])
-
-    # activations used
-    st.markdown('*Activations Used: *')
-    st.code(report['Activations'].values[0])
-
-    # Number of Epochs:
-    st.markdown('*Number of Epochs: *')
-    st.code(report['epochs'].values[0])
-
-    # Metrics
-    st.markdown('*Metrics: *')
-    st.code(report['metrics'].values[0])
-    
-    # Train_Accuracy and Test Accuracy 
-    st.markdown('*Train and Test Accuracy : *')
-    st.code('Train - {:.2f}, Test - {:.2f}'.format(report['Train_Accuracy'].values[0],report['Test_Accuracy'].values[0]))
-
-    # Elapsed 
-    st.markdown('*Elapsed Time for Training : *')
-    st.code(report['elapsed'].values[0])
+    for i in range(len(d)-1):
+        st.markdown(d[i])
+        st.code(report[cols[i]].values[0])
 
 
     st.markdown('***')
+
+    expander = st.beta_expander('Source-Code')
+    expander.markdown('_Ipynb_ translated into _PDF_ using _LaTeX_')
+    expander.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=64 height=64>](https://github.com/r0han99/Deep-Learning-Anatomy/raw/main/Projects/MNIST/Pytorch/MNIST-Pytorch.pdf) <small style='color:blue;'><i>Download</i></small>'''.format(img_to_bytes('./ipynb.png')), unsafe_allow_html=True)
+        
+
+def cs_plots(report):
+
+    st.markdown("<h3 style='font-family: Avenir; font-weight:bold; font-size:30px;'>Visualisation of Train Cycle <br>( Epoch vs Metric/Loss )</h3>",unsafe_allow_html=True)
+    
+    path = report['plots'].values[0]
+
+    
+    for name in glob.glob(path+'/*.png'):
+        st.image(name)
         
 
     
@@ -152,18 +128,32 @@ def cs_main():
     st.markdown('***')
     
     if not isinstance(report, pd.DataFrame):
-        st.markdown("There are no traces of a report.csv file under the **{}** implementation in the project _dir_, There's a possibility that the project is unfinished or missing files required to render the subject. until then try other projects.".format(framework))
+        st.markdown("There are no traces of a _report.csv_ file under the ***{}*** implementation in the project _dir_, There's a possibility that the project is unfinished or missing files required to render the subject. until then try other projects.".format(framework))
+
+    
     else:
-        temp_desc = report['Desc'][0]
+        temp_desc = report['desc'][0]
         st.markdown("<h3 style='font-family: century gothic'>Project Title : <bold><strong>{}</strong></bold></h3>".format(project),unsafe_allow_html=True)
         st.markdown('**Description** ~ _{}_'.format(temp_desc))
         st.markdown('**Implementation** ~ ___{}___'.format(framework))
         
         st.markdown('***')
-        cs_body(report)
+
+        options = st.sidebar.radio('View', ('Project Artefacts','Plots'), key='web-page-definition')
+        if options == 'Project Artefacts':
+            cs_body(report)
+        if options == 'Plots':
+            cs_plots(report)
+        
+
+        st.sidebar.markdown('***')
+        expander = st.sidebar.beta_expander('Want to try?')
+        expander.warning('Currently Under-Beta')
+        if expander.checkbox('yes?'):
+            model_eval('model')
 
 
-    model_eval('model')
+    
    
     
 
@@ -178,4 +168,5 @@ def cs_main():
 
 if __name__ == '__main__':
     cs_main()
+    st.sidebar.markdown('***')
     st.sidebar.markdown('''[<img src='data:image/png;base64,{}' class='img-fluid' width=64 height=64>](https://github.com/r0han99/) <small><i>Database of My Knowledge</i></small>'''.format(img_to_bytes('./tesseract.png')), unsafe_allow_html=True)
