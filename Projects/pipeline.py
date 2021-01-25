51a694d1fd55c64a51646efd554aee30720d521a
r0han99 <shikataganai.955@gmail.com>
2021-01-26 03:41:05 +0530

    patch_num-2
    - Added functionalities to pipeline script
    - created variable_dict and prompt_dict
    - now the prompts under the project artefacts are predefined and dynamically generated
    - segmented artefacts and plots into radios
    - added download-link to the source-code pdf

--- Projects/pipeline.py
+++ Projects/pipeline.py
@@ -5,11 +5,11 @@ import pickle
 from pathlib import Path
 import sys
 import os
+import datetime
 # handeling deprecated warnings 
 import warnings
 
 warnings.filterwarnings("ignore", category=UserWarning)
-
 
 
 
@@ -26,7 +26,8 @@ def init_report():
     pass
 
 
-def generate_report(project_path, project_name,report):
+def generate_report(project_path, project_name, fw, report):
+    date = datetime.datetime.now().strftime('%d-%A (%Y)')
 
     # creates an Anchor point for the Application c
     if os.path.exists(project_path+'report.csv'):
@@ -34,12 +35,22 @@ def generate_report(project_path, project_name,report):
     else:
         report.to_csv(project_path+'report.csv')
         print('report created!')
-        print('filling catalog .. ')
-        with open('../catalog.csv','a+') as f:
-            f.write('{},{}\n'.format(project_name,'./Projects/'+project_path[2:]+'report.csv'))
+        print('Updating Catalog .. ')
+        if not os.path.exists('../catalog.csv'):
+            print('No catalog file in existence, creating one, writing content..')
+            with open('../catalog.csv','w') as f:
+                f.write('{},{},{},{}\n'.format('date','project','framework','path'))
+            # write content
+            with open('../catalog.csv','a+') as f:
+                f.write('{},{},{},{}\n'.format(date,project_name,fw,'./Projects/'+project_path[2:]+'report.csv'))
+                print('Done!')
         
-        print('Done!')
-        
+        else:
+            with open('../catalog.csv','a+') as f:
+                f.write('{},{},{},{}\n'.format(date,project_name,fw,'./Projects/'+project_path[2:]+'report.csv'))
+            
+            print('Done!')
+            
 
 def validate_meta():
 
@@ -49,7 +60,7 @@ def validate_meta():
 
 
 
-def meta_collect(project_path,project_name):
+def meta_collect(project_path,project_name,fw):
     '''
 
 
@@ -61,21 +72,21 @@ def meta_collect(project_path,project_name):
     '''
     print('Meta Data Collection and Organisation')
 
-    meta = os.path.join(project_path,'state_dict.txt')
+    meta = os.path.join(project_path,'artefacts.txt')
     if os.path.exists(meta):
-        print('state_dict.txt exists, extracting content and remodelling .. ')
-        with open('./MNIST/Pytorch/state_dict.txt', 'rb') as f: 
+        print('artefacts.txt exists, extracting content and remodelling .. ')
+        with open('./MNIST/Pytorch/artefacts.txt', 'rb') as f: 
              data = f.read() 
   
         d = pickle.loads(data) 
         report = pd.DataFrame(pd.Series(d)).T
         
         # report.csv creation
-        generate_report(project_path, project_name, report)
+        generate_report(project_path, project_name,fw, report)
         
 
     else:
-        print('state_dict is not found, create network-metadata before triggering pipeline')
+        print('artefacts.txt is non-existent, create network-metadata before triggering pipeline')
 
 
     '''
@@ -103,7 +114,28 @@ def path_creation(project_path):
         
     path_url = os.path.join(project_path,fw)
     print('Project Path ~ ', path_url)
-    return path_url
+    return path_url,fw
+
+
+
+def pickle_handle(file_path,method,d=None):
+
+    if method == 'dump':
+        print('Dumping records!')
+        with open(file_path,'wb') as f:
+            pickle.dump(d,f)
+        print(f'byte-code dump created at {file_path}')
+    
+    elif method == 'load':
+
+        print('Loading Byte-code text!')
+        with open(file_path, 'rb') as f:
+            data = f.read() 
+        d = pickle.loads(data)
+
+        return d
+
+
 
 
 def main():
@@ -111,13 +143,13 @@ def main():
     if sys.argv[1] == '-np':
         
         try:
-            if sys.argv[2]: 
+            if len(sys.argv[2]) == 2: 
                 abs_path = sys.argv[2]
                 project_name = sys.argv[2].split('/')[1]
                 print('Project Directory - {}'.format(project_name))
-                project_path = path_creation(abs_path)
+                project_path,fw = path_creation(abs_path)
                 
-                meta_collect(project_path,project_name) # Data collection subroutine 
+                meta_collect(project_path,project_name,fw) # Data collection subroutine 
         
         except:
             print("There's no Absolute Path given in the arguments! ")
@@ -140,13 +172,64 @@ def main():
                     continue
 
                 # Data collection subroutine 
-            project_path = path_creation(abs_path)
-            meta_collect(project_path,project_name)
+            project_path,fw = path_creation(abs_path)
+            meta_collect(project_path,project_name,fw)
                 
 
            
+    elif sys.argv[1] == '-varl' or sys.argv[1] == '--var-list':
 
-   
+        # read here 
+        d = pickle_handle(file_path='./var-dict.txt',method='load')
+
+        if len(sys.argv) == 2:
+
+            print('Default Variable List : ')
+            for var,dtype in zip(d.keys(), d.values()):
+                print('{:}-----{:}'.format(var,dtype))
+
+            # display it 
+        else:
+            if sys.argv[2] == '--update':
+                # Load the pickled prompt_list, display it 
+                # update with new prompts required
+                print('Adding new variable and dtype to var-dict')
+                variable = input('Enter variable to add into dictionary: ')
+                d[variable] = input('Enter Dtype of that variable: ')
+                print('Variable Added to the Dictionary! -- saving state .. ')
+                pickle_handle(file_path='./var-dict.txt',method='dump',d=d)
+
+            elif sys.argv[2] == '--pop':
+                # popping variable 
+                print('Popping Existing variable and dtype from var-dict')
+                variable = input('Enter variable to add into dictionary: ')
+                try:
+                    d.pop(variable)
+                    print(f'{variable} deleted from the records! -- saving state..')
+                    pickle_handle(file_path='./var-dict.txt',method='dump',d=d)
+
+                except:
+                    print('Invalid var-name')
+                    exit(1)
+
+
+            else:
+                print('Invalid follow up argument detect, -varl is followed by --update')
+                exit(1)
+
+    elif sys.argv[1] == '--status':
+        print('prints the catalog, segmented according to the framework')
+
+
+    elif sys.argv[1] == '-sample' or sys.argv[1] == '-sc':
+        print('Sample Collection to display the type of data handeled ')
+
+    elif len(sys.argv) == 1:
+
+        print('This Script Takes in Command-Line Arguments, triggering the functionalitied required, try --help for assistance')
+        exit(0)
+
+        
 
     
             
