0d842c59378775e45ec1330a9c15d180206276fe
Rohan Sai <45916202+r0han99@users.noreply.github.com>
2021-02-11 13:58:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ def img_to_bytes(img_path):
 def model_predict(model_path,x,true,class_names):
         
         st.write(model_path)
-#         model = load_model(model_path)
+        model = load_model(model_path)
 #         predict = model.predict_classes(x)
 #         if class_names == None:
 #             st.markdown('Model Classifies this to be - `{}`'.format(predict[0]))
