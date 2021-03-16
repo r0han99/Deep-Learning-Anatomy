39dd681318118eecd7197b0982695068a931d17d
Rohan Sai <45916202+r0han99@users.noreply.github.com>
2021-03-16 23:11:14 +0530

    updated title

--- src/simulation.py
+++ src/simulation.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 def app():
 
-    st.markdown("<h3 style='font-family: BioRhyme; color:#E45E18; '>Dynamic Visualisation of Intermediate Activations of each Layer in a Convolutional Neural Network</h3>",unsafe_allow_html=True)
+    st.markdown("<h3 style='font-family: BioRhyme; color:#E45E18; '>Dynamic Visualisation of Intermediate Layer Activations in a Convolutional Neural Network Architecture</h3>",unsafe_allow_html=True)
     st.markdown("_TensorFlow Playground ripoff & François Chollet's `Deep Learning with Python` Inspiration_")
 
     st.image('./pngs/Post-Activation.png')
